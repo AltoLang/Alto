56f9fc1551062b59d2f20e765866e992db05c060
FilipToth <realFilipToth@gmail.com>
2021-03-25 20:08:11 +0100

    Made sure that the last line was being added to the SourceText.

--- Accel/CodeAnalysis/Text/SourceText.cs
+++ Accel/CodeAnalysis/Text/SourceText.cs
@@ -68,7 +68,7 @@ namespace Accel.CodeAnalysis.Text
                 }
             }
 
-            if (position > lineStart)
+            if (position >= lineStart)
                 AddLine(result, sourceText, position, lineStart, 0);
 
             return result.ToImmutable();
