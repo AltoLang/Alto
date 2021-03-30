6dd31c0d7de41378ad6b25789617e4c34a0dd7d8
FilipToth <realFilipToth@gmail.com>
2021-03-30 15:21:40 +0200

    Fix parser tests.

--- Alto.Tests/CodeAnalysis/Syntax/ParserTests.cs
+++ Alto.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -18,11 +18,11 @@ namespace Alto.Tests.CodeAnalysis.Syntax
             var op1Text = SyntaxFacts.GetText(op1);
             var op2Text = SyntaxFacts.GetText(op2);
             var text = $"a {op1Text} b {op2Text} c";
-            var expression = SyntaxTree.Parse(text).Root;
+            var expression = ParseExpression(text);
 
             if (op1Precedence >= op2Precedence)
             {
-                using (var e = new AssertingEnumerator(expression.Expression))
+                using (var e = new AssertingEnumerator(expression))
                 {
                     e.AssertNode(SyntaxKind.BinaryExpression);
                     e.AssertNode(SyntaxKind.BinaryExpression);
@@ -38,7 +38,7 @@ namespace Alto.Tests.CodeAnalysis.Syntax
             } 
             else
             {
-                using (var e = new AssertingEnumerator(expression.Expression))
+                using (var e = new AssertingEnumerator(expression))
                 {
                     e.AssertNode(SyntaxKind.BinaryExpression);
                     e.AssertNode(SyntaxKind.NameExpression);
@@ -63,14 +63,14 @@ namespace Alto.Tests.CodeAnalysis.Syntax
             var unaryText = SyntaxFacts.GetText(unaryKind);
             var binaryText = SyntaxFacts.GetText(binaryKind);
             var text = $"{unaryText} a {binaryText} b";
-            var expression = SyntaxTree.Parse(text).Root;
+            var expression = ParseExpression(text);
 
             Debug.Assert(unaryText != null);
             Debug.Assert(binaryText != null);
 
             if (unaryPrecedence <= binaryPrecedence)
             {
-                using (var e = new AssertingEnumerator(expression.Expression))
+                using (var e = new AssertingEnumerator(expression))
                 {
                     e.AssertNode(SyntaxKind.BinaryExpression);
                     e.AssertNode(SyntaxKind.UnaryExpression);
@@ -84,7 +84,7 @@ namespace Alto.Tests.CodeAnalysis.Syntax
             } 
             else
             {
-                using (var e = new AssertingEnumerator(expression.Expression))
+                using (var e = new AssertingEnumerator(expression))
                 {
                     e.AssertNode(SyntaxKind.UnaryExpression);
                     e.AssertToken(unaryKind, unaryText);
@@ -96,6 +96,13 @@ namespace Alto.Tests.CodeAnalysis.Syntax
                     e.AssertToken(SyntaxKind.IdentifierToken, "b");
                 }
             }
+        }
+
+        public static ExpressionSyntax ParseExpression(string text)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+            var root = syntaxTree.Root;
+            return root.Expression;
         }
 
         public static IEnumerable<object[]> GetUnaryOperatorPairsData()
