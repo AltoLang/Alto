d027fa5b970293760e701151e24965bceca158c4
FilipToth <realFilipToth@gmail.com>
2021-06-03 15:41:19 +0200

    Evaluate functions, change diagnostics, change lowerer

--- Alto/CodeAnalysis/Lowering/Lowerer.cs
+++ Alto/CodeAnalysis/Lowering/Lowerer.cs
@@ -199,7 +199,7 @@ namespace Alto.CodeAnalysis.Lowering
             
             var variableDeclaration = new BoundVariableDeclaration(node.Variable, node.LowerBound);
             var variableExpression = new BoundVariableExpression(node.Variable);
-            var upperBoundSymbol = new VariableSymbol("upperBound", true, TypeSymbol.Int);
+            var upperBoundSymbol = new LocalVariableSymbol("upperBound", true, TypeSymbol.Int);
             var upperBoundDeclaration = new BoundVariableDeclaration(upperBoundSymbol, node.UpperBound);
             var condition = new BoundBinaryExpression(
                 variableExpression,
