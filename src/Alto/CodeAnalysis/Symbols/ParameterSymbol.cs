33b25b07c3f5135d819731d3bf547513e60361aa
Filip Toth <68369112+FilipToth@users.noreply.github.com>
2021-06-29 09:06:20 -0400

    Change `ParameterSyntax`

--- src/Alto/CodeAnalysis/Symbols/ParameterSymbol.cs
+++ src/Alto/CodeAnalysis/Symbols/ParameterSymbol.cs
@@ -1,12 +1,24 @@
+using Alto.CodeAnalysis.Binding;
+
 namespace Alto.CodeAnalysis.Symbols
 {
     public sealed class ParameterSymbol : LocalVariableSymbol
     {
-        public ParameterSymbol(string name, TypeSymbol type)
+        public ParameterSymbol(string name, TypeSymbol type, bool isOptional = false)
             : base(name: name, isReadOnly: true, type: type)
         {
+            IsOptional = isOptional;
         }
 
+        internal ParameterSymbol(string name, TypeSymbol type, bool isOptional, BoundExpression optionalValue = null)
+            : base(name: name, isReadOnly: true, type: type)
+        {
+            IsOptional = isOptional;
+            OptionalValue = optionalValue;
+        }
+
+        public bool IsOptional { get; }
+        internal BoundExpression OptionalValue { get; }
         public override SymbolKind Kind => SymbolKind.Parameter;
     }
 }
