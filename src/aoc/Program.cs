fb6885baa7b1d4ce4cdf6d787e757c61b993edf3
Filip Toth <68369112+FilipToth@users.noreply.github.com>
2021-07-14 15:06:46 -0400

    Basic "compiler" cli

--- src/aoc/Program.cs
+++ src/aoc/Program.cs
@@ -5,11 +5,37 @@ using System.Linq;
 using Alto.CodeAnalysis;
 using Alto.CodeAnalysis.Symbols;
 using Alto.CodeAnalysis.Syntax;
+using Alto.IO;
 
 namespace Alto
 {
     internal static class Program
     {
-        
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Usage: ac <source-paths>");
+                return;
+            }
+    
+            if (args.Length > 1)
+            {
+                Console.Error.WriteLine("Currently, only one path is supported.");
+                return;
+            }
+
+            var path = args.Single();
+            var text = File.ReadAllText(path);
+
+            var syntaxTree = SyntaxTree.Parse(text);
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
+            if (result.Diagnostics.Any())
+                DiagnosticsWriter.WriteDiagnostics(Console.Out, result.Diagnostics, syntaxTree);
+            else
+                Console.WriteLine(result.Value);
+        }
     }
 }
