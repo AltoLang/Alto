9e5ec57bb48d468d918dc4761f5fa4f24934f09d
Filip Toth <68369112+FilipToth@users.noreply.github.com>
2021-07-23 12:43:51 -0400

    Update BoundGlobalScope.cs

--- src/Alto/CodeAnalysis/Binding/BoundGlobalScope.cs
+++ src/Alto/CodeAnalysis/Binding/BoundGlobalScope.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using Alto.CodeAnalysis.Symbols;
 using Alto.CodeAnalysis.Syntax;
@@ -8,7 +9,7 @@ namespace Alto.CodeAnalysis.Binding
     {
         public BoundGlobalScope(BoundGlobalScope previous, ImmutableArray<Diagnostic> diagnostics, 
             ImmutableArray<FunctionSymbol> functions, ImmutableArray<VariableSymbol> variables, 
-            ImmutableArray<BoundStatement> statements, ImmutableArray<SyntaxTree> importedTrees)
+            ImmutableArray<BoundStatement> statements, Dictionary<SyntaxTree, IEnumerable<SyntaxTree>> importedTrees)
         {
             Previous = previous;
             Diagnostics = diagnostics;
@@ -23,6 +24,6 @@ namespace Alto.CodeAnalysis.Binding
         public ImmutableArray<FunctionSymbol> Functions { get; }
         public ImmutableArray<VariableSymbol> Variables { get; }
         public ImmutableArray<BoundStatement> Statements { get; }
-        public ImmutableArray<SyntaxTree> ImportedTrees { get; }
+        public Dictionary<SyntaxTree, IEnumerable<SyntaxTree>> ImportedTrees { get; }
     }
 }
