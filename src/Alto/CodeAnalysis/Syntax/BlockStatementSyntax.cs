9df2b8efe86035348208608e7e03ef65e49ad223
Filip Toth <68369112+FilipToth@users.noreply.github.com>
2021-08-06 14:24:10 -0400

    Parse nested functions

--- src/Alto/CodeAnalysis/Syntax/BlockStatementSyntax.cs
+++ src/Alto/CodeAnalysis/Syntax/BlockStatementSyntax.cs
@@ -4,16 +4,21 @@ namespace Alto.CodeAnalysis.Syntax
 {
     public sealed class BlockStatementSyntax : StatementSyntax
     {
-        public BlockStatementSyntax(SyntaxTree syntaxTree, SyntaxToken openBraceToken, ImmutableArray<StatementSyntax> statements, SyntaxToken closeBraceToken)
+        public BlockStatementSyntax(SyntaxTree syntaxTree, SyntaxToken openBraceToken, 
+                                    ImmutableArray<StatementSyntax> statements,
+                                    ImmutableArray<FunctionDeclarationSyntax> functions,
+                                    SyntaxToken closeBraceToken)
             : base(syntaxTree)
         {
             OpenBraceToken = openBraceToken;
             Statements = statements;
+            Functions = functions;
             CloseBraceToken = closeBraceToken;
         }
 
         public SyntaxToken OpenBraceToken { get; }
         public ImmutableArray<StatementSyntax> Statements { get; }
+        public ImmutableArray<FunctionDeclarationSyntax> Functions { get; }
         public SyntaxToken CloseBraceToken { get; }
 
         public override SyntaxKind Kind => SyntaxKind.BlockStatement;
