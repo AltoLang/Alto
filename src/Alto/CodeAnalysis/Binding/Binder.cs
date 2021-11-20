53f70f8f6ae46d20d204173c9404bad1031ed240
Filip Toth <filip.toth@accelley.com>
2021-11-20 20:14:49 +0100

    Add ordinals to `ParameterSymbol`

--- src/Alto/CodeAnalysis/Binding/Binder.cs
+++ src/Alto/CodeAnalysis/Binding/Binder.cs
@@ -187,8 +187,10 @@ namespace Alto.CodeAnalysis.Binding
             var parameters = ImmutableArray.CreateBuilder<ParameterSymbol>();
             var seenParameterNames = new HashSet<string>();
 
-            foreach (var parameterSyntax in syntax.Parameters)
+            for (int i = 0; i < syntax.Parameters.Count; i++)
             {
+                var parameterSyntax = syntax.Parameters[i];
+                
                 var paramName = parameterSyntax.Identifier.Text;
                 var paramType = BindTypeClause(parameterSyntax.Type);
                 var isOptional = parameterSyntax.IsOptional;
@@ -207,7 +209,7 @@ namespace Alto.CodeAnalysis.Binding
                         optionalExpression = BindConversion(expression, paramType, parameterSyntax.OptionalExpression.Location);
                     }
                     
-                    var parameter = new ParameterSymbol(paramName, paramType, isOptional, optionalExpression);
+                    var parameter = new ParameterSymbol(paramName, paramType, i, isOptional, optionalExpression);
                     parameters.Add(parameter);
                 }
             }
